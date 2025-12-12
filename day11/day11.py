58ed51d8137d4062302a5d221f261cf3212c62cd
Dimeji <dimejiogunyoye@googlemail.com>
2025-12-12 04:17:29 +0000

    Day 11 - part 2

--- day11/day11.py
+++ day11/day11.py
@@ -22,11 +22,24 @@ def __traverse_devices(devices, output) -> int:
     return result
 
 
-def __traverse_devices_tracking_path(devices, path, output) -> int:
+def __traverse_devices_tracking_path(devices, output, dac, fft, memo) -> int:
     if output == "out":
-        return 1
+        if dac and fft:
+            return 1
+        return 0
 
-    return 0
+    state = (output, dac, fft)
+    if state in memo:
+        return memo[state]
+
+    result = 0
+    for o in devices[output]:
+        found_dac = True if o == "dac" else dac
+        found_fft = True if o == "fft" else fft
+        result += __traverse_devices_tracking_path(devices, o, found_dac, found_fft, memo)
+
+    memo[state] = result
+    return result
 
 
 def part_one(data) -> int:
@@ -36,13 +49,13 @@ def part_one(data) -> int:
 
 def part_two(data) -> int:
     devices = __parse_devices(data)
-    return __traverse_devices(devices, "svr")
+    return __traverse_devices_tracking_path(devices,"svr", False, False, {})
 
 
 def main() -> int:
     with open(DATA) as f:
         data = f.read()
-        # print("Part 1: " + str(part_one(data)))
+        print("Part 1: " + str(part_one(data)))
         print("Part 2: " + str(part_two(data)))
     return 0
 
